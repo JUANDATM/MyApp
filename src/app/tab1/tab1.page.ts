b1bb2b29ac0618649c06869100fc778bbe76c640
JUANDATM <kira00711@hotmail.com>
2020-02-17 17:04:17 -0600

    debugger

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NumberFormatStyle } from '@angular/common';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  ngOnInit() {
+    let numero = 10;
+    // tslint:disable-next-line:no-debugger
+    debugger;
+
+    numero += 10;
+    numero *= 10;
+    numero -= 10;
+
+    console.log( {numero});
+  }
 
   constructor() {}
 
